44abc3e15ba3c680efc78496bbb92e75679f7763
lennel02 <lennelthreian@gmail.com>
2022-02-17 11:52:44 +0800

    Add graph

--- diabetesApp.py
+++ diabetesApp.py
@@ -1,13 +1,57 @@
-import streamlit as st
 import numpy as np
 import pandas as pd
 import joblib
 import os
 import seaborn as sns
+import time
 
+
+@st.cache(suppress_st_warning=True)
+def load_csv_data(file_dir, head=0, tail=0):
+    df = pd.read_csv(file_dir)
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+    for i in range(100):
+        latest_iteration.text(f'Loading CSV Data... {i+1}%')
+        bar.progress(i+1)
+        time.sleep(0.01)
+    bar.empty()
+    latest_iteration.text('')
+    if head > 0 and tail == 0:
+        return df.head(head)
+    elif head == 0 and tail > 0:
+        return df.head(tail)
+    return df
+
+
+def load_chart(data, kind):
+    if kind == 'line':
+        st.write("Line Chart")
+        st.line_chart(data)
+    elif kind == 'area':
+        st.write("Area Chart")
+        st.area_chart(df[columns])
+    elif kind == 'bar':
+        st.write("Bar Chart")
+        st.bar_chart(df[columns])
+    else:
+        st.write("Line Chart")
+        st.line_chart(data)
+        
+
+#st.set_page_config(layout="wide")
 st.title("Diabetes Predictor App")
 st.write("From the diabetes data, we built a machine learning model for diabetes predictions.")
 
+# Initialize CSV data
+filename = "diabetes_classification.csv"
+df = load_csv_data(filename, head=20)
+
+# Initialize columns and target
+columns = ['Glucose', 'BMI', 'Age', 'BloodPressure']
+target = 'Outcome'
+
+# Sidebar
 st.sidebar.title("Diabetes Predictor App Parameters")
 st.sidebar.write("Tweak to change predictions")
 
@@ -23,12 +67,28 @@ age = st.sidebar.slider("Age", 0, 150, 15)
 # Blood Pressure
 blood_pressure = st.sidebar.slider("Blood Pressure", 0, 300, 100)
 
+# Dataframe visibility
+st.sidebar.subheader("Data Frame Visibility")
+option_sidebar = st.sidebar.checkbox("Hide")
+if not option_sidebar:
+    st.caption(f"Data Frame: '{filename}'")
+    st.write(df)
+    st.write("\n\n")
+    
+# Line chart
+load_chart(df[columns], "line")
+
+# Area chart
+load_chart(df[columns], "area")
+
+# Bar chart
+load_chart(df[columns], "bar")
 
 # Main Page
 st.subheader("Predictions")
 
 # Loading the model
-filename = 'finalized_model.sav'
+filename = 'finalized_model_diabetes.sav'
 loaded_model = joblib.load(filename)
 
 # [Glucose, BMI, Age, BloodPressure]
